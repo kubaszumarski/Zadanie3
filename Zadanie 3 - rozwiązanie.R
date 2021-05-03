a3f7e7dcb0e5e3d57f358b69e35895f70edb4864
kubaszumarski <kuba@szumarski.pl>
2021-05-03 15:21:31 +0200

    3 zadania zrobione

--- Zadanie 3 - rozwiązanie.R
+++ Zadanie 3 - rozwiązanie.R
@@ -38,5 +38,56 @@ rankAccountBigDatatoChunk("konta.csv",1000, "occupation", "NAUCZYCIEL","saldo",1
 
 #przyklad naglowka:
 #rankAccountBigDatatoChunk(filename = "usersAccounts.csv", 1000,"occupation", "NAUCZYCIEL", "saldo",10)
+
+
 #3.SPRAWIDZIC CZY DA SIE ZROBIC TO SAMO W zapytaniu SQL dla takich wartosci jak: tabelaZbazyDanych,occupation, nauczyciel, saldo
+library(DBI)
+library(RMySQL)
+library(rstudioapi)
+
+#połaczenie z bazą
+connectMe<-function(typ=MySQL(), dbname="big_data", host="localhost", user="root"){
+  con<-dbConnect(typ,
+                 dbname=dbname,
+                 host=host,
+                 user=user,
+                 password=askForPassword("database password"))
+}
+
+con<-connectMe()
+#zapis danych z pliku do bazy chunkami
+writeToDataBase<- function (connection,filepath, tablename, size, header = TRUE,  sep=",", deleteTable=TRUE){
+  ap=!deleteTable
+  ov=deleteTable
+  
+  fileConnection<-file(description = filepath,open = "r")
+  dbConn<-dbConnect(connection)
+  data<-read.table(fileConnection,nrows = size,header = header,fill=TRUE,sep=sep)
+  columnNames<-names(data)
+  dbWriteTable(conn = dbConn, name=tablename, data, append=ap, overwrite=ov)
+  
+  repeat{
+    if(nrow(data)==0){
+      close(fileConnection)
+      dbDisconnect(dbConn)
+      break
+    }
+    data<-read.table(fileConnection,nrows = size,col.names = columnNames,fill=TRUE,sep=sep)
+    dbWriteTable(conn = dbConn, name=tablename, data, append=TRUE, overwrite=FALSE)
+    
+  }
+}
+#import do bazy
+writeToDataBase(con,'konta.csv','konta',1000)
+
+con<-connectMe()
+#zapytanie do bazy -funkcja
+queryDatabase<-function(connection, table, colName, groupName, valueSort, num){
+  df<-dbGetQuery(connection, paste0("SELECT * from  ",table," where ",colName,"='",groupName,"' order by ",valueSort," desc limit ",num," ;" )) 
+  View(df)
+}
+
+#wykonanie przykładowego zapytania
+queryDatabase(con,'konta','occupation','NAUCZYCIEL','saldo',10)
+#ODP.: Da się.
 ######################################################################
